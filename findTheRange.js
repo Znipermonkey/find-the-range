862f1490af4df51c12559171ee126b1a05f91641
Robert Free <144733501+Znipermonkey@users.noreply.github.com>
2023-09-18 09:25:08 -0600

    simplified the code

--- findTheRange.js
+++ findTheRange.js
@@ -5,21 +5,13 @@ let array = [28, 43, -12, 30, 4, 0, -36]
 
 
 let getMin = 0
-
+let getMax = 0
 for(let i = 1; i < array.length; i++){
     if(array[i] < getMin){
         getMin = array[i]
-    }
-}
-console.log (getMin)
-
-let getMax = 0
-for(let i = 1; i < array.length; i++){
-    if(array[i] > getMax){
+    }else if(array[i] > getMax){
         getMax = array[i]
     }
 }
-console.log (getMax)
 
-let range = getMax - getMin
-console.log(range)
+console.log (getMin, getMax)
